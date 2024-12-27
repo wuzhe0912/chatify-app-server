7cdb31dbb963821c42993a601940efacffcb3db8
Pitt Wu <kgb00128@gmail.com>
2024-12-28 07:09:08 +0800

    [Chore] setting prettier

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+  res.send('Register');
+};
+
+export const login = (req, res) => {
+  res.send('Login');
+};
+
+export const logout = (req, res) => {
+  res.send('Logout');
+};
